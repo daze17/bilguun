90fa2bba952d235b3d12c1db5fa7a66cd93e925b
baasansuren <bilguun889@gmail.com>
2021-04-23 23:26:22 +0800

    closebutton

--- components/SideDrawer/CloseButton.js
+++ components/SideDrawer/CloseButton.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FaTimes } from 'react-icons/fa';
 import styles from './CloseButton.module.css'
 
 const CloseButton = ({ click }) => (
   <div className={styles.container}>
     <button className={styles.toggle_button} onClick={click} >
-      <FontAwesomeIcon icon={faTimes} size="3x"/>
+       <FaTimes size='3em' />
     </button>
   </div>
 )
